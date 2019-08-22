60479fbaac16697e2b34c011a852e4d8f7997dfe
v891262 <cleon.s.clarke@verizon.com>
2019-08-22 14:28:31 +0000

    String Methods

---
+++ strings/stringmethod01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+lilstring = "Alta3 Research offers classes on pythin coding"
+newlist = lilstring.split(" ")
+print(newlist)
+
+myiplist = ["192", "168", "0", "12"]
+
+singleip = ".".join(myiplist)
+
+print(singleip)
+
+
