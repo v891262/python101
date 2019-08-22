4a0d97bacac9aab058f6988f67183a45a990fc42
v891262 <cleon.s.clarke@verizon.com>
2019-08-22 16:21:39 +0000

    Testing with IF

---
+++ if-test2/iftest2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+#ipchk = "192.168.0.1"
+ipchk = input("Apply an IP address:")
+
+##if ipchk:
+##    print("Looks like the IP address was set to:" + ipchk)
+##else:
+##    print("You did not provide input.")
+if ipchk=="192.168.70.1":
+    print("Looks like the IP address of the Gateway was set:" + ipchk + "This is not recommended")
+elif ipchk:
+    print("Looks like the IP address was set:" + ipchk)
+else:
+    print("You did not provide input.")
+
